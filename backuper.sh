e56a75173070e3b0c8e2b06bbab397b86cb64f2a
Mehrdad <122950903+Alfred-13@users.noreply.github.com>
2025-11-01 18:06:22 +0330

    Update backuper.sh

--- backuper.sh
+++ backuper.sh
@@ -38,74 +38,7 @@ check_root() {
     [[ $EUID -eq 0 ]] || error "This script must be run as root"
 }
 
-detect_package_manager() {
-    if command -v apt-get &>/dev/null; then
-        echo "apt"
-    elif command -v dnf &>/dev/null; then
-        echo "dnf"
-    elif command -v yum &>/dev/null; then
-        echo "yum"
-    elif command -v pacman &>/dev/null; then
-        echo "pacman"
-    else
-        error "Unsupported package manager"
-    fi
-}
 
-update_os() {
-    local package_manager=$(detect_package_manager)
-    log "Updating the system using $package_manager..."
-    
-    case $package_manager in
-        apt)
-            apt-get update -y && apt-get upgrade -y || error "Failed to update the system"
-            ;;
-        dnf|yum)
-            $package_manager update -y || error "Failed to update the system"
-            ;;
-        pacman)
-            pacman -Syu --noconfirm || error "Failed to update the system"
-            ;;
-    esac
-    success "System updated successfully"
-}
-
-install_dependencies() {
-    local package_manager=$(detect_package_manager)
-    local packages=("wget" "zip" "cron" "msmtp" "mutt")
-
-    log "Installing dependencies: ${packages[*]}..."
-    
-    case $package_manager in
-        dnf|yum)
-            packages+=("mariadb")
-            $package_manager install -y "${packages[@]}" || error "Failed to install dependencies"
-            ;;
-        pacman)
-            packages+=("mariadb")
-            pacman -Sy --noconfirm "${packages[@]}" || error "Failed to install dependencies"
-            ;;
-    esac
-    success "Dependencies installed successfully"
-}
-
-install_yq() {
-    if command -v yq &>/dev/null; then
-        success "yq is already installed."
-        return
-    fi
-
-    log "Installing yq..."
-    local ARCH=$(uname -m)
-    local YQ_BINARY="yq_linux_amd64"
-
-    [[ "$ARCH" == "aarch64" || "$ARCH" == "arm64" ]] && YQ_BINARY="yq_linux_arm64"
-
-    wget -q "https://github.com/mikefarah/yq/releases/latest/download/$YQ_BINARY" -O /usr/bin/yq || error "Failed to download yq."
-    chmod +x /usr/bin/yq || error "Failed to set execute permissions on yq."
-
-    success "yq installed successfully."
-}
 menu() {
     update_os
     install_dependencies
@@ -1166,3 +1099,4 @@ main() {
 }
 
 main
+
